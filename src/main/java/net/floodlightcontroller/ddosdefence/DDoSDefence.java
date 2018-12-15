0159e8f429c5df49f3a3f6962930576223e87518
Antonio Le Caldare <antonio.lecaldare@gmail.com>
2018-12-15 09:48:07 +0100

    controller: added code to send a rule when a tcp packet with the protected service address and port is sent to the controller. the rule contains the same packet contraints and does not change packets header

--- src/main/java/net/floodlightcontroller/ddosdefence/DDoSDefence.java
+++ src/main/java/net/floodlightcontroller/ddosdefence/DDoSDefence.java
@@ -5,8 +5,19 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.projectfloodlight.openflow.protocol.OFFlowAdd;
 import org.projectfloodlight.openflow.protocol.OFMessage;
+import org.projectfloodlight.openflow.protocol.OFPacketIn;
 import org.projectfloodlight.openflow.protocol.OFType;
+import org.projectfloodlight.openflow.protocol.action.OFAction;
+import org.projectfloodlight.openflow.protocol.action.OFActionSetField;
+import org.projectfloodlight.openflow.protocol.action.OFActions;
+import org.projectfloodlight.openflow.protocol.match.Match;
+import org.projectfloodlight.openflow.protocol.match.MatchField;
+import org.projectfloodlight.openflow.protocol.oxm.OFOxms;
+import org.projectfloodlight.openflow.types.EthType;
+import org.projectfloodlight.openflow.types.IPv4Address;
+import org.projectfloodlight.openflow.types.OFPort;
 import org.restlet.Context;
 import org.restlet.Restlet;
 import org.restlet.routing.Router;
@@ -24,6 +35,7 @@ import net.floodlightcontroller.core.web.ControllerSwitchesResource;
 import net.floodlightcontroller.core.web.LoadedModuleLoaderResource;
 import net.floodlightcontroller.packet.Ethernet;
 import net.floodlightcontroller.packet.IPv4;
+import net.floodlightcontroller.packet.TCP;
 import net.floodlightcontroller.restserver.IRestApiService;
 import net.floodlightcontroller.restserver.RestletRoutable;
 
@@ -32,6 +44,10 @@ public class DDoSDefence implements IOFMessageListener,IFloodlightModule,IDDoSDe
 	
 	// REST Interface
 	protected IRestApiService restApiService;
+
+	// Parameters
+	IPv4Address protectedServiceAddress;
+	OFPort protectedServicePort;
 
 	// Statistics
 	final static int CONNECTIONS_THRESHOLD = 100;
@@ -93,12 +109,58 @@ public class DDoSDefence implements IOFMessageListener,IFloodlightModule,IDDoSDe
 
 	@Override
 	public Command receive(IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
+		OFPacketIn pi = (OFPacketIn)msg;
+
 		Ethernet eth = IFloodlightProviderService.bcStore.get(cntx, IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
-		IPv4 ipv4 = (IPv4)eth.getPayload();
-		
-		// need to filter only http files?
-	
-		return Command.CONTINUE;
+
+		// filter non TCP packets
+		if(!(eth.getPayload() instanceof IPv4))
+			return Command.CONTINUE;
+		IPv4 ipv4Msg = (IPv4)eth.getPayload();
+
+		if(!(ipv4Msg.getPayload() instanceof TCP))
+			return Command.CONTINUE;
+		TCP tcpMsg = (TCP)ipv4Msg.getPayload();
+
+		// filter packets sent to other services
+		if(!(tcpMsg.getDestinationPort().equals(protectedServicePort)))
+			return Command.CONTINUE;
+
+		// new MATCH list (ipv4 traffic to the protected server)
+		// add rule for (src:srcport -> dstaddress:address)
+		Match.Builder mb = sw.getOFFactory().buildMatch();
+		mb.setExact(MatchField.ETH_TYPE, EthType.IPv4);
+		mb.setExact(MatchField.TCP_SRC, tcpMsg.getSourcePort());
+		mb.setExact(MatchField.IPV4_SRC, ipv4Msg.getSourceAddress());
+		mb.setExact(MatchField.IPV4_DST, protectedServiceAddress);
+
+		// new RULE
+		OFFlowAdd.Builder fmb = sw.getOFFactory().buildFlowAdd();
+		fmb.setBufferId(pi.getBufferId());
+		fmb.setXid(pi.getXid());
+
+		// new ACTION LIST
+		OFActions actions = sw.getOFFactory().actions();
+		ArrayList<OFAction> actionList = new ArrayList<OFAction>();
+		OFOxms oxms = sw.getOFFactory().oxms();
+
+		// Rule can change IP destination address of packets
+		OFActionSetField setDlDst = actions.buildSetField()
+				.setField(
+					oxms.buildIpv4Dst()
+					.setValue(protectedServiceAddress)
+					.build()
+					).build();
+		actionList.add(setDlDst);
+
+		// attach ACTION LIST to RULE
+		fmb.setActions(actionList);
+		fmb.setMatch(mb.build());
+
+		// send the ACTION
+		sw.write(fmb.build());
+
+		return Command.STOP;
 	}
 
 	@Override
