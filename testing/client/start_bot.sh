884337e0ce7f60906f842d93ffec3e8b1ab17b8a
Antonio Le Caldare <antonio.lecaldare@gmail.com>
2018-12-24 07:21:28 +0100

    start_bot.sh: don't output the HTTP response. output format is now similar to start_client.sh

--- testing/client/start_bot.sh
+++ testing/client/start_bot.sh
@@ -3,6 +3,15 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 while true; do
-	$SCRIPTPATH/gen_http_keepalive.sh | nc $1 $2 -w 2
-	echo "connection dropped. Retrying..."
+	REQUEST_RESULT=$($SCRIPTPATH/gen_http_keepalive.sh | nc $1 $2 -w 2);
+	if [ $? -eq 1 ]; then
+		echo "Connection dropped or reached timeout"
+		continue;
+	fi
+
+	if [[ "$REQUEST_RESULT" == *"<html>"* ]]; then
+		echo "Got Regular HTTP Response"
+	else
+		echo "Got Forwarding HTTP Response. I can't forward! (I'm stupid)"
+	fi
 done
